b50b0507eb4c756087c2c76d271c2c9c95dc9a94
granko <gransky.emanuel@gmail.com>
2026-03-13 20:49:59 +0100

    Reading input

--- longestArithmeticSubsequence.cpp
+++ longestArithmeticSubsequence.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
+    int n;
+    vector<int> sequence;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int number;
+        cin >> number;
+        sequence.push_back(number);
+    }
 
     return 0;
 }
